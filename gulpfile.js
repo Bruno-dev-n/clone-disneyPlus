b3f80d2848674bae06038fcec189999df949881e
positivo <developnobre@gmail.com>
2024-05-24 12:51:21 -0300

    criação do hero, e importação de fonte avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,24 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function style() {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({
             outputStyle:'compressed'
         }))
+        .on('error', sass.logError)
         .pipe(gulp.dest('./dist/styles/'));
 }
+function images(){
+    return gulp.src('./src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'));
+}
 
-exports.default = style;
+exports.default = gulp.parallel (style, images);
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', {
         ignoreInitial: false
-    }, gulp.parallel(style));
+    }, gulp.parallel(style, images));
 }
